2cdef4bb616d5e9774742be7f5be1e96a571727c
James Ade <60360836+JamesHardey@users.noreply.github.com>
2025-05-23 17:31:44 +0100

    update

--- pages/api/generate-pdf.ts
+++ pages/api/generate-pdf.ts
@@ -4,6 +4,9 @@ import { invoiceTemplate } from '../../utils/invoiceTemplate';
 import { format } from 'date-fns';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if(req.method === 'GET'){
+    return res.status(200).json({ message: 'Hello World' });
+  }
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method not allowed' });
   }
